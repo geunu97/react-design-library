133a31d3a9f52052a6c01110f4963d4dac02b9db
geunu97 <pgw@hanryubank.com>
2023-06-14 10:36:22 +0900

    chore: semantic-release 설정

---
+++ release.config.js
@@ -0,0 +1,16 @@
+/** semantic-release 설정 파일
+ * git 커밋 히스토리를 분석하고, 버전을 자동으로 생성하며, 해당 버전을 npm에 배포
+ *
+ * 커밋 메시지 컨벤션 규칙 따라야 함 (https://www.conventionalcommits.org/en/v1.0.0/)
+ * 커밋 메시지 규칙에 맞게 버전을 자동으로 결정
+ */
+
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/npm",
+  ],
+};
