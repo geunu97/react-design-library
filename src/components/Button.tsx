5efe4210d1588a8545d9dea46b97ebf3e000934a
geunu97 <pgw@hanryubank.com>
2023-06-15 22:51:36 +0900

    fix: styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import styles from "../styles/example.css";
+// import styles from "../styles/example.css";
+import "../styles/example.css";
 
 function Button() {
   return <div className="example">버튼입니다.</div>;
