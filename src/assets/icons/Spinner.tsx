dfce711042fdd20c36052d05d20b5b49de17036a
geunu97 <pgw@hanryubank.com>
2023-07-10 13:45:34 +0900

    fix: add alert component

---
+++ src/assets/icons/Spinner.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface SpinnerIconProps {
+  width: string;
+  height: string;
+  duration?: string;
+}
+
+const SpinnerIcon = (props: SpinnerIconProps) => {
+  return (
+    <svg
+      role="presentation"
+      width={props.width}
+      height={props.height}
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <g fill="none" fillRule="evenodd">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M6.343 17.657A8 8 0 1 0 4 12"
+        >
+          <animateTransform
+            attributeName="transform"
+            attributeType="XML"
+            type="rotate"
+            dur={props.duration ? props.duration : "1s"}
+            from="0 12 12"
+            to="360 12 12"
+            repeatCount="indefinite"
+          ></animateTransform>
+        </path>
+      </g>
+    </svg>
+  );
+};
+
+export default SpinnerIcon;
---
+++ src/components/molecules/Loading/Loading.stories.ts
@@ -0,0 +1,88 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Loading from "./Loading";
+
+const meta: Meta<typeof Loading> = {
+  title: "Components/Molecules/Loading",
+  component: Loading,
+};
+
+export default meta;
+export type Story = StoryObj<typeof Loading>;
+
+/* -------------------------------- Color -------------------------------- */
+export const SuccessColor: Story = {
+  name: "Color: Success",
+  args: {
+    color: "success",
+  },
+};
+export const WarningColor: Story = {
+  name: "Color: Warning",
+  args: {
+    color: "warning",
+  },
+};
+export const DangerColor: Story = {
+  name: "Color: Danger",
+  args: {
+    color: "danger",
+  },
+};
+export const InfoColor: Story = {
+  name: "Color: Info",
+  args: {
+    color: "info",
+  },
+};
+export const NeutralColor: Story = {
+  name: "Color: Neutral",
+  args: {
+    color: "neutral",
+  },
+};
+export const BlackColor: Story = {
+  name: "Color: Black",
+  args: {
+    color: "black",
+  },
+};
+
+/* -------------------------------- Size -------------------------------- */
+export const SmallestSize: Story = {
+  name: "Size: Smallest",
+  args: {
+    size: "smallest",
+  },
+};
+export const SmallerSize: Story = {
+  name: "Size: Smaller",
+  args: {
+    size: "smaller",
+  },
+};
+export const MediumSize: Story = {
+  name: "Size: Medium",
+  args: {
+    size: "medium",
+  },
+};
+export const LargerSize: Story = {
+  name: "Size: Larger",
+  args: {
+    size: "larger",
+  },
+};
+export const LargestSize: Story = {
+  name: "Size: Largest",
+  args: {
+    size: "largest",
+  },
+};
+
+/* -------------------------------- Percentage -------------------------------- */
+export const Percentage: Story = {
+  name: "Percentage : number",
+  args: {
+    percentage: 72,
+  },
+};
