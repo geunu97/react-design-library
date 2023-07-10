ac1be21a5f2dc88c7d63639c84605bd5134a83b5
geunu97 <pgw@hanryubank.com>
2023-07-10 13:52:27 +0900

    fix: export loading component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as GButton } from "./components/atoms/Button/Button";
 export { default as GAlert } from "./components/molecules/Alert/Alert";
+export { default as GLoading } from "./components/molecules/Loading/Loading";
 export { default as GHeader } from "./components/organisms/Header/Header";
