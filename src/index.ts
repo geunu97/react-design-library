537092024cbf0cf0f2e95645758c8b4b3cb44638
geunu <olo90632951@gmail.com>
2024-03-17 21:41:01 +0900

    fix: export 경로 수정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default as GProvider } from './components/common/Provider';
 export { default as showGAlert } from './components/molecules/Alert/showAlert';
 export { default as showGToast } from './components/molecules/Toast/showToast';
-export { default as GButton } from './components/atoms/Button';
 export { default as GLoading } from './components/molecules/Loading';
+export { default as GButton } from './components/atoms/Button';
+export { default as GLabel } from './components/atoms/Label';
 export { default as GHeader } from './components/organisms/Header';
