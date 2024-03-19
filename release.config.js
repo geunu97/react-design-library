069612a36af7e2818cd058aad0e2aeb06c51f749
geunu <olo90632951@gmail.com>
2024-03-20 06:49:48 +0900

    ci: npm release dev서버 추가

--- release.config.js
+++ release.config.js
@@ -1,7 +1,8 @@
 /** semantic-release 설정 파일
  * git 커밋 히스토리를 분석하고, 버전을 자동으로 생성하며, 해당 버전을 npm에 배포
  *
- * dev브랜치를 통해 prerelease 가능하도록 설정
+ * dev브랜치를 통해 prerelease 가능하도록 설정 (배포된 버전은 다음과 같이 사용가능)
+ * npm install @geunwoo/react-design-system@dev
  * 
  * 커밋 메시지 컨벤션 규칙 따라야 함 (https://www.conventionalcommits.org/en/v1.0.0/)
  * 커밋 메시지 규칙에 맞게 버전을 자동으로 결정
