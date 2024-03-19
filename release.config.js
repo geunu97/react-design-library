d8383e5efdb0477535638aa8bfee7034eba5e47e
geunu <olo90632951@gmail.com>
2024-03-20 06:32:29 +0900

    ci: npm release dev서버 추가

--- release.config.js
+++ release.config.js
@@ -10,7 +10,7 @@
 module.exports = {
   branches: [
     "main",
-    { name: "dev", prerelease: "dev" }
+    { name: "dev", prerelease: true }
   ],
   plugins: [
     "@semantic-release/commit-analyzer",
